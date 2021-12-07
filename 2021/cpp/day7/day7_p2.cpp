290e09a71f46af0833107709ff20e58e9908901d
Pavel Danov <pavel.danov@outlook.com>
2021-12-07 12:50:24 +0000

    More efficient solution for day 7 part 2

--- 2021/cpp/day7/day7_p2.cpp
+++ 2021/cpp/day7/day7_p2.cpp
@@ -38,8 +38,11 @@ int main(int argc, char** argv[])
 		for (int i = *minPos; i <= *maxPos; ++i)
 		{
 			auto dist = std::abs(c - i);
-			for (int x = 0, j = dist; j >= 0; ++x, --j)
-				fuelFreq[i] += x;
+			fuelFreq[i] += (dist * (dist + 1) / 2); // https://en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
+			
+			// Original brute force sumation
+			//for (int x = 0, j = dist; j >= 0; ++x, --j)
+			//	fuelFreq[i] += x;
 		}
 	}
 
